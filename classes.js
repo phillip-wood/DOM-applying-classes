32faa2eb6d7f2e9cabce4c31c88ef200803b902d
phillip wood <phillip.wood369@gmail.com>
2020-09-10 09:31:59 +1200

    add js code to add styling to html div's and make visible

--- classes.js
+++ classes.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()
+  three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -18,8 +20,22 @@ function one () {
   one.classList.add('blue')
 }
 
-// CREATE FUNCTION two HERE
+function two () {
+  
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 
-// CREATE FUNCTION three HERE
+function three () {
+  
+  var three = document.getElementById('three')
+  three.classList.add('black')
+}
 
-// CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.add('visible')
+
+
+}
